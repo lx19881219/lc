59c77c1e60c85e59a96a1f8e1627874f32f19c42
Xiang Li <lx19881219@gmail.com>
2015-04-10 00:31:04 -0400

    P4-not done

--- ac_fourty.py
+++ ac_fourty.py
@@ -1,8 +1,25 @@
-class tree:
-    def __init__(self, x):
+import pdb
+
+class tnode:
+    def __init__(self, x, left=None, right=None):
 	self.val = x
-	self.left = None
-	self.right = None	
+	self.left = left
+	self.right = right
+
+    def insert_left(self, x):
+	self.left = tnode(x)
+	return self.left
+
+    def insert_right(self, x):
+	self.right = tnode(x)
+	return self.right
+
+class tree:
+    def __init__(self):
+	self.root = None
+
+    def insert(self, data, left, right):
+	self.root = tnode(data, left, right)	
 
 class solution:
     """
@@ -37,12 +54,114 @@ class solution:
     
     The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
     """
-    
+
+    def max_depth(self, root):
+	#recursively
+	'''left = 0
+	if root.left:
+	    left = self.max_depth(root.left)
+	right = 0
+	if root.right:
+	    right = self.max_depth(root.right)
+	return max(left, right) + 1'''
+	if root == None:
+	    return 0
+	return max(self.max_depth(root.left), self.max_depth(root.right)) + 1
+
+    def max_depth_iter(self, root):
+	#iteraction
+	res = 0
+	left = 0
+	right = 0	
+        stack = []
+	stack.append([root, 1])
+	while len(stack) > 0:
+	    node, cur_depth = stack.pop()
+	    res = max(res, cur_depth)
+	    if node.left != None:
+		stack.append([node.left, cur_depth+1])
+	    if node.right != None:
+		stack.append([node.right, cur_depth+1])
+	return res
+
+    def two_sum_ii(self, arr, target):
+	#Greedy Algorithm: search from both end.
+	if len(arr) < 2:
+	    return None
+	i = 0
+	j = len(arr) - 1
+	while(i < j):
+	    if target > arr[i] + arr[j]:
+		i += 1
+	    elif target < arr[i] + arr[j]:
+		j -= 1
+	    if target == arr[i] + arr[j]:
+		return [i, j]
+	return None 
+
+    def same_tree(self, root1, root2):
+	#recursively
+	if root1 == None and root2 == None:
+	    print 'c1'
+	    return True
+	'''if root1 == None or root2 == None:
+	    print 'c2'
+	    return False
+	if root1.val != root2.val:
+	    return False'''
+	if root1 and root2 and root1.val == root2.val:
+	    print 'c3', root1.val
+	    return  self.same_tree(root1.left, root2.left) and self.same_tree(root1.right, root2.left)
+	return False
+
     def print_result(self, function, src, res):
         print '{0}\nInput: {1}\nOutput {2}\n'.format(function, src, res)
+
+    def print_tree(self, root):
+	print root.val
+	if root.left:
+	    print 'left'
+	    self.print_tree(root.left)
+	if root.right:
+	    print 'right'
+	    self.print_tree(root.right)
+	return True
     
 if __name__ == "__main__":
+
     arr = [1,2,3,4,2,1,3]
     result1 = solution().single_number_dict(arr)
     solution().print_result('Single Number', arr, result1)
 
+    root = tnode(0)
+    n1 = root.insert_left(3)
+    n2 = root.insert_right(5)
+    n3 = n1.insert_left(7)
+    n4 = n1.insert_right(9)
+    n5 = n4.insert_left(1)
+    n6 = n2.insert_left(2)
+    """
+		0
+	    3	    5
+	  7   9   2
+	     1
+	
+    """
+    solution().print_tree(root)
+    result2 = solution().max_depth_iter(root)
+    solution().print_result('Max Depth', root.val, result2)
+
+    arr = [1,5,8,12,24]
+    result3 = solution().two_sum_ii(arr, 17)
+    solution().print_result('Two Sum II', arr, result3)
+
+    root1 = tnode(0)
+    n7 = root1.insert_left(3)
+    n8 = root1.insert_right(5)
+    n9 = n7.insert_left(7)
+    n10 = n7.insert_right(9)
+    n11 = n10.insert_left(1)
+    n12 = n8.insert_left(2)
+    solution().print_tree(root1)
+    result4 = solution().same_tree(root, root1)
+    solution().print_result('Same Tree', 0, result4)
