abf720ba5c45f34a8b8176356998829c089a7830
Xiang Li <lx19881219@gmail.com>
2015-04-24 00:21:00 -0400

    first round 002

--- lc_first_round.py
+++ lc_first_round.py
@@ -1,3 +1,18 @@
+class lnode:
+
+    def __init__(self, value=None):
+	self.val = value
+	self.nxt = None
+
+    def setNext(self, node):
+	self.nxt = node
+
+    def print_list(self, head):
+	if head != None:
+	    print head.val,
+	    return self.print_list(head.nxt)
+	print ''
+
 class solution:
     """
     Given an array of integers, find two numbers such that they add up to a specific target number.
@@ -20,6 +35,36 @@ class solution:
 	    	d[arr[i]] = i
 	return None
 
+    """
+    You are given two linked lists representing two non-negative numbers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+    Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+    Output: 7 -> 0 -> 8
+
+    """
+    def addTwoNums(self, num1, num2):
+	head = lnode()
+	flag = 0
+	cur = head
+	while num1 and num2:
+	    cur.nxt = lnode((num1.val + num2.val + flag) % 10)
+	    flag = (num1.val + num2.val + flag) / 10
+	    num1 = num1.nxt
+	    num2 = num2.nxt
+	    cur = cur.nxt
+	while num1:
+	    cur.nxt = lnode((num1.val + flag) % 10)
+	    flag = (num1.val + flag) / 10
+	    num1 = num1.nxt
+	    cur = cur.nxt
+	while num2:
+	    cur.nxt = lnode((num2.val + flag) % 10)
+	    flag = (num2.val + flag) / 10
+	    num2 = num2.nxt
+	    cur = cur.nxt    
+	if flag == 1:
+	    cur.nxt = lnode(1)
+	return head.nxt	
     def p(self, function, src, res):
 	print '{0}\nInput: {1}\nOutput {2}\n'.format(function, src, res)
 
@@ -28,3 +73,19 @@ if __name__ == "__main__":
     arr = [2, 7, 11, 15]
     result1 = s.two_sum(arr, 9)
     s.p('Two Sum', arr, result1)
+
+    ln1 = lnode(2)
+    ln2 = lnode(4)
+    ln3 = lnode(3)
+    ln1.setNext(ln2)
+    ln2.setNext(ln3)
+    lnode().print_list(ln1)
+    ln4 = lnode(5)
+    ln5 = lnode(6)
+    ln6 = lnode(4)
+    ln4.setNext(ln5)
+    ln5.setNext(ln6)
+    lnode().print_list(ln4)
+    result2 = s.addTwoNums(ln1, ln4)
+    lnode().print_list(result2)
+    
